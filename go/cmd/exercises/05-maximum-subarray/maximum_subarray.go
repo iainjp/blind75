fa493ceb0eefd473c87eb459b8b6e941aa000fe0
Iain Pritchard <iainpritchard92@gmail.com>
2025-04-11 22:06:47 +0100

    Add solution for 05-maximum-subarray

--- go/cmd/exercises/05-maximum-subarray/maximum_subarray.go
+++ go/cmd/exercises/05-maximum-subarray/maximum_subarray.go
@@ -1,5 +1,15 @@
 package exercises
 
 func maxSubArray(nums []int) int {
-	return 0
+	// Kadane's algorithm
+
+	bestSum := -100000
+	currentSum := 0
+
+	for _, num := range nums {
+		currentSum = max(num, currentSum+num)
+		bestSum = max(currentSum, bestSum)
+	}
+
+	return bestSum
 }
