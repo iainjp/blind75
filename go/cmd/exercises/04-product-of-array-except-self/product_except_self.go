1373dca81fd88c9c25f694310efc0abb66bec468
Iain Pritchard <iainpritchard92@gmail.com>
2025-04-11 19:23:32 +0100

    Solution for 04-product-except-self, passing tests

--- go/cmd/exercises/04-product-of-array-except-self/product_except_self.go
+++ go/cmd/exercises/04-product-of-array-except-self/product_except_self.go
@@ -3,18 +3,21 @@ package exercises
 func productExceptSelf(nums []int) []int {
 	// track product per index
 	products := make([]int, len(nums))
-	for i := range products {
-		products[i] = 1
+
+	// keeping track of the accumulated product
+	leftProduct := 1
+	rightProduct := 1
+
+	// starting left, track running product and assign to `products[i]`,
+	for i := 0; i < len(nums); i++ {
+		products[i] = leftProduct
+		leftProduct *= nums[i]
 	}
 
-	for i, val := range nums {
-		for otherI := range len(nums) {
-			if otherI == i {
-				continue
-			}
-
-			products[otherI] = val * products[otherI]
-		}
+	// start right, track running product and multiply and assign to `products[i]`
+	for i := len(nums) - 1; i >= 0; i-- {
+		products[i] = products[i] * rightProduct
+		rightProduct *= nums[i]
 	}
 
 	return products
