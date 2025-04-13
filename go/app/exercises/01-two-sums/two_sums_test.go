5803052a6ceaa356576dd2d232fcaf3ea2f8cd74
Iain Pritchard <iainpritchard92@gmail.com>
2025-04-13 18:36:05 +0100

    Capturing benchmark results in Reporter

--- go/app/exercises/01-two-sums/two_sums_test.go
+++ go/app/exercises/01-two-sums/two_sums_test.go
@@ -7,16 +7,17 @@ import (
 	"github.com/iainjp/blind75/utils"
 )
 
+var testCases = []struct {
+	input  []int
+	target int
+	want   []int
+}{
+	{[]int{2, 7, 11, 15}, 9, []int{0, 1}},
+	{[]int{3, 2, 4}, 6, []int{1, 2}},
+	{[]int{3, 3}, 6, []int{0, 1}},
+}
+
 func TestTwoSums(t *testing.T) {
-	var testCases = []struct {
-		input  []int
-		target int
-		want   []int
-	}{
-		{[]int{2, 7, 11, 15}, 9, []int{0, 1}},
-		{[]int{3, 2, 4}, 6, []int{1, 2}},
-		{[]int{3, 3}, 6, []int{0, 1}},
-	}
 
 	for testNum, test := range testCases {
 		testName := fmt.Sprintf("Case [%v]", testNum)
@@ -24,5 +25,11 @@ func TestTwoSums(t *testing.T) {
 			got := twoSum(test.input, test.target)
 			utils.CheckEqual(got, test.want, t)
 		})
+	}
+}
+
+func BenchmarkTwoSums(b *testing.B) {
+	for b.Loop() {
+		_ = twoSum(testCases[0].input, testCases[0].target)
 	}
 }
--- go/app/reporter/reporter.go
+++ go/app/reporter/reporter.go
@@ -6,26 +6,46 @@ import (
 	"encoding/json"
 	"fmt"
 	"os/exec"
+	"slices"
+	"strings"
 	"time"
 )
 
-// Example:
+// Examples:
 // {"Time":"2025-04-13T15:32:28.46481+01:00","Action":"start","Package":"github.com/iainjp/blind75/utils"}
+// {"Time":"2025-04-13T16:35:06.05039+01:00","Action":"pass","Package":"github.com/iainjp/blind75/app/exercises/03-contains-duplicates","Test":"TestContainsDuplicates","Elapsed":0}
+// {"Time":"2025-04-13T16:35:06.050743+01:00","Action":"output","Package":"github.com/iainjp/blind75/app/exercises/03-contains-duplicates","Output":"PASS\n"}
+// {"Time":"2025-04-13T16:35:06.051109+01:00","Action":"output","Package":"github.com/iainjp/blind75/app/exercises/03-contains-duplicates","Output":"ok  \tgithub.com/iainjp/blind75/app/exercises/03-contains-duplicates\t0.151s\n"}
+// {"Time":"2025-04-13T16:35:06.051121+01:00","Action":"pass","Package":"github.com/iainjp/blind75/app/exercises/03-contains-duplicates","Elapsed":0.151}
+
+// Struct containing results of a golang test
 type TestResult struct {
 	Time    time.Time `json:"Time"`
 	Action  string    `json:"Action"`
 	Package string    `json:"Package"`
 	Output  string    `json:"Output"`
-	Elapsed int       `json:"Elapsed"`
+	Elapsed float64   `json:"Elapsed"`
+	Test    string    `json:"Test"`
 }
 
-func Report() {
+type TestSummary struct {
+	Package     string
+	AvgElapsed  float64
+	BytesPerOp  string
+	AllocsPerOp string
+	NsPerOp     string
+	TestCount   int
+	TestPass    int
+	TestFail    int
+}
+
+func RunTests() []TestResult {
 	binary, lookErr := exec.LookPath("go")
 	if lookErr != nil {
 		panic(lookErr)
 	}
 
-	args := []string{"test", "-json", "./..."}
+	args := []string{"test", "-bench=.", "-benchmem", "-json", "./..."}
 
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
@@ -41,12 +61,96 @@ func Report() {
 
 	results := []TestResult{}
 
+	wantedActions := []string{"pass", "fail", "output"}
 	scanner := bufio.NewScanner(&stdout)
 	for scanner.Scan() {
-		r := TestResult{}
-		json.Unmarshal(scanner.Bytes(), &r)
-		results = append(results, r)
+		tr := TestResult{}
+		json.Unmarshal(scanner.Bytes(), &tr)
+
+		if slices.Contains(wantedActions, tr.Action) {
+			results = append(results, tr)
+		}
 	}
 
-	fmt.Println(results)
+	return results
+}
+
+// Benchmark result
+// {... "Output":"BenchmarkTwoSums-8   \t104114722\t        11.21 ns/op\t      16 B/op\t       1 allocs/op\n"}
+
+func summarize(results []TestResult) []TestSummary {
+	groupedByExercise := map[string][]TestResult{}
+	for _, result := range results {
+		// skip project tests unrelated to exercises
+		if !strings.Contains(result.Package, "exercises") {
+			continue
+		}
+		val, ok := groupedByExercise[result.Package]
+		if !ok {
+			val = []TestResult{}
+		}
+
+		groupedByExercise[result.Package] = append(val, result)
+	}
+
+	reports := []TestSummary{}
+
+	for pkg, results := range groupedByExercise {
+		elapsed := []float64{}
+		passes := 0
+		fails := 0
+		var nsPerOp string
+		var bytesPerOp string
+		var allocsPerOp string
+
+		// grab relevant details from results
+		for _, res := range results {
+			switch res.Action {
+			case "pass":
+				if !strings.Contains(res.Test, "Benchmark") && strings.Contains(res.Test, "Case") {
+					passes += 1
+					elapsed = append(elapsed, res.Elapsed)
+				}
+			case "fail":
+				fails += 1
+			}
+
+			// identify relevant benchmark results
+			if strings.Contains(res.Test, "Benchmark") && strings.Contains(res.Output, "ns/op") {
+				split := strings.Split(res.Output, "\t")
+				nsPerOp = strings.TrimSpace(split[2])
+				bytesPerOp = strings.TrimSpace(split[3])
+				allocsPerOp = strings.TrimSpace(split[4])
+			}
+		}
+
+		totalElapsed := 0.0
+		for _, e := range elapsed {
+			totalElapsed += e
+		}
+
+		report := TestSummary{
+			Package:     pkg,
+			AvgElapsed:  totalElapsed / float64(len(elapsed)),
+			BytesPerOp:  bytesPerOp,
+			NsPerOp:     nsPerOp,
+			AllocsPerOp: allocsPerOp,
+			TestCount:   passes + fails,
+			TestPass:    passes,
+			TestFail:    fails,
+		}
+
+		reports = append(reports, report)
+	}
+
+	return reports
+}
+
+func Report() {
+	results := RunTests()
+	summaries := summarize(results)
+
+	for _, s := range summaries {
+		fmt.Printf("%+v\n", s)
+	}
 }
