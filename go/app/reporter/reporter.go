b1b27050d6ef49b328aec8cbe2d10717e4d7a2ed
Iain Pritchard <iainpritchard92@gmail.com>
2025-04-13 16:13:56 +0100

    Capture test output, deser to struct

--- go/app/reporter/reporter.go
+++ go/app/reporter/reporter.go
@@ -1,10 +1,23 @@
 package app
 
 import (
-	"os"
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"os/exec"
-	"syscall"
+	"time"
 )
+
+// Example:
+// {"Time":"2025-04-13T15:32:28.46481+01:00","Action":"start","Package":"github.com/iainjp/blind75/utils"}
+type TestResult struct {
+	Time    time.Time `json:"Time"`
+	Action  string    `json:"Action"`
+	Package string    `json:"Package"`
+	Output  string    `json:"Output"`
+	Elapsed int       `json:"Elapsed"`
+}
 
 func Report() {
 	binary, lookErr := exec.LookPath("go")
@@ -12,12 +25,28 @@ func Report() {
 		panic(lookErr)
 	}
 
-	cmdArgs := []string{"go", "test", "-json", "./..."}
+	args := []string{"test", "-json", "./..."}
 
-	env := os.Environ()
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	testCmd := exec.Command(binary, args...)
+	testCmd.Stdout = &stdout
+	testCmd.Stderr = &stderr
 
-	execErr := syscall.Exec(binary, cmdArgs, env)
+	execErr := testCmd.Run()
 	if execErr != nil {
+		fmt.Printf("Error running tests. Stderr: %v", stderr.String())
 		panic(execErr)
 	}
+
+	results := []TestResult{}
+
+	scanner := bufio.NewScanner(&stdout)
+	for scanner.Scan() {
+		r := TestResult{}
+		json.Unmarshal(scanner.Bytes(), &r)
+		results = append(results, r)
+	}
+
+	fmt.Println(results)
 }
