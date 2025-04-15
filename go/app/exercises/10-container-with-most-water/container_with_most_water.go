8b19dbdd576e3ed92dcde7c073eea5280cf4f14c
Iain Pritchard <iainpritchard92@gmail.com>
2025-04-15 14:01:23 +0100

    Optimisations, still short of performance required

--- go/app/exercises/10-container-with-most-water/container_with_most_water.go
+++ go/app/exercises/10-container-with-most-water/container_with_most_water.go
@@ -3,7 +3,7 @@ package exercises
 func maxArea(height []int) int {
 	// two heights, only one solution
 	if len(height) == 2 {
-		return max(height[0], height[1]) // removes *1 which is the length of container
+		return min(height[0], height[1]) // removes length as it's 1
 	}
 
 	maxSize := 0
@@ -11,9 +11,22 @@ func maxArea(height []int) int {
 	for left := range height {
 		right := len(height) - 1
 
+		maxHeightConsidered := min(left, right)
+
 		// until we've considered all candidates, calculate size, set if max, move right in 1
 		for left < right {
-			size := (right - left - 1) * height[right]
+			// skip if we've already considered a heigher value
+			if min(left, right) < maxHeightConsidered {
+				right -= 1
+				continue
+			}
+
+			maxHeightConsidered = min(left, right)
+
+			l := right - left
+			// take min of two heights, as water will overflow, not slant
+			h := min(height[left], height[right])
+			size := l * h
 			maxSize = max(size, maxSize)
 			right -= 1
 		}
--- go/app/exercises/10-container-with-most-water/container_with_most_water_test.go
+++ go/app/exercises/10-container-with-most-water/container_with_most_water_test.go
@@ -13,6 +13,9 @@ var testCases = []struct {
 }{
 	{[]int{1, 8, 6, 2, 5, 4, 8, 3, 7}, 49},
 	{[]int{1, 1}, 1},
+	{[]int{4, 3, 2, 1, 4}, 16},
+	{[]int{1, 2}, 1},
+	{[]int{8, 7, 2, 1}, 7},
 }
 
 func TestContainerWithMostWater(t *testing.T) {
