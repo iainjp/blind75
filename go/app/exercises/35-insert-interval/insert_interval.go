75808d5dd566376683f77ee2a57295a6ac68e873
Iain Pritchard <iainpritchard92@gmail.com>
2025-04-27 15:40:20 +0100

    Blegh, got the interval logic wrong. Will return to this

--- go/app/exercises/35-insert-interval/insert_interval.go
+++ go/app/exercises/35-insert-interval/insert_interval.go
@@ -1,5 +1,35 @@
 package exercises
 
+import "slices"
+
 func insert(intervals [][]int, newInterval []int) [][]int {
-	return [][]int{}
+	const LOWER, UPPER = 0, 1
+
+	// result := [][]int{}
+
+	var insertInterval = func(intervals [][]int, newInterval []int) [][]int {
+		result := [][]int{}
+
+		for _, existingInterval := range intervals {
+			// determine if we have an overlap -- yeah this is bogus.
+			if newInterval[LOWER] <= existingInterval[LOWER] || newInterval[UPPER] >= existingInterval[UPPER] {
+				// we have an overlap; merge it
+				combined := append(newInterval, existingInterval...)
+				newLower := slices.Min(combined)
+				newUpper := slices.Max(combined)
+				result = append(result, []int{newLower, newUpper})
+			} else {
+				// otherwise, add existing interval
+				result = append(result, existingInterval)
+			}
+		}
+
+		return result
+	}
+
+	// I think this requires multiple iterations, to resolve new overlaps as they get generated ...
+
+	inserted := insertInterval(intervals, newInterval)
+
+	return inserted
 }
