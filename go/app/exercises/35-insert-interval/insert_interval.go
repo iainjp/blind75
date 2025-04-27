4de4b3e70dc5bcedab8fecfbebfe33b22c8a0a63
Iain Pritchard <iainpritchard92@gmail.com>
2025-04-27 19:59:52 +0100

    Fix merge function, tracking last-inserted and comparing end values

--- go/app/exercises/35-insert-interval/insert_interval.go
+++ go/app/exercises/35-insert-interval/insert_interval.go
@@ -1,35 +1,38 @@
 package exercises
 
-import "slices"
+import (
+	"slices"
+)
 
 func insert(intervals [][]int, newInterval []int) [][]int {
 	const LOWER, UPPER = 0, 1
 
-	// result := [][]int{}
+	var mergeIntervals = func(intervals [][]int) [][]int {
+		// sort by first element in intervals
+		var firstElemCmp = func(firstInterval []int, secondInterval []int) int {
+			return firstInterval[0] - secondInterval[0]
+		}
+		slices.SortFunc(intervals, firstElemCmp)
 
-	var insertInterval = func(intervals [][]int, newInterval []int) [][]int {
-		result := [][]int{}
+		merged := [][]int{intervals[0]}
 
-		for _, existingInterval := range intervals {
-			// determine if we have an overlap -- yeah this is bogus.
-			if newInterval[LOWER] <= existingInterval[LOWER] || newInterval[UPPER] >= existingInterval[UPPER] {
-				// we have an overlap; merge it
-				combined := append(newInterval, existingInterval...)
-				newLower := slices.Min(combined)
-				newUpper := slices.Max(combined)
-				result = append(result, []int{newLower, newUpper})
+		for _, interval := range intervals[1:] {
+			lastInserted := merged[len(merged)-1]
+
+			// no overlap, just add it
+			if lastInserted[UPPER] < interval[LOWER] {
+				merged = append(merged, interval)
 			} else {
-				// otherwise, add existing interval
-				result = append(result, existingInterval)
+				// overlapping intervals, possibly extend last inserted by taking max of both uppers
+				lastInserted[UPPER] = max(lastInserted[UPPER], interval[UPPER])
 			}
 		}
 
-		return result
+		return merged
 	}
 
-	// I think this requires multiple iterations, to resolve new overlaps as they get generated ...
+	intervals = append(intervals, newInterval)
+	merged := mergeIntervals(intervals)
 
-	inserted := insertInterval(intervals, newInterval)
-
-	return inserted
+	return merged
 }
